b4dfd85de901ee349bc75d27b61dcdbffa638297
Liam Doherty <liamdoherty845@gmail.com>
2026-06-27 15:07:59 +0800

    favicon, title

--- apps/web-app/src/Layout.tsx
+++ apps/web-app/src/Layout.tsx
@@ -4,7 +4,8 @@ const Layout: ParentComponent = (props) => {
     return (
         <div class="min-h-screen flex flex-col bg-base-100">
             <div class="navbar bg-base-200 shadow-sm px-4">
-                <span class="text-xl font-bold tracking-tight">Pixel Wizard</span>
+                <img src="hat.png" width="40px" height="40px" />
+                <span class="pixelify-sans-700">Pixel Wizard</span>
             </div>
             <div class="flex flex-1">{props.children}</div>
         </div>
