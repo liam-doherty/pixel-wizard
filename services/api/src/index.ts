b4b09cefd7dbaf17b7a3f2f7bb6f1e3b92dd7522
Liam Doherty <liamdoherty845@gmail.com>
2026-06-29 23:21:50 +0800

    azure deploy

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -6,7 +6,7 @@ const app = makeApi()
 serve(
     {
         fetch: app.fetch,
-        port: 3000,
+        port: Number(process.env.PORT ?? 3000),
     },
     (info) => {
         console.log(`Server is running on http://localhost:${info.port}`)
