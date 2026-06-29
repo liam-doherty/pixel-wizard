2b6d206fba1e6509ed5b4b6aacb64d411ccedc52
Liam Doherty <liamdoherty845@gmail.com>
2026-06-29 14:27:50 +0800

    utilise catalog, clearup cached git files

--- libs/common/dist/main.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const isEmpty: (data: any) => boolean;
-export declare const isObject: (data: any) => any;
-export declare const isBlank: (data: any) => any;
-export declare const helloWorld: (name?: string) => string;
-export * from './player';
--- services/api/dist/api.d.ts
+++
@@ -1,3 +0,0 @@
-import { Hono } from 'hono';
-export declare function makeApi(): Hono<import("hono/types").BlankEnv, import("hono/types").BlankSchema, "/">;
-//# sourceMappingURL=api.d.ts.map
